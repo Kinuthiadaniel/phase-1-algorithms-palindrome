0dc7cd2f0fb46742b6fa5552873688c1878686f6
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-04-12 10:35:58 +0300

    changes

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === word.split("").reverse().join("")){
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  input: radar
+  output: true
+  input: motherboard
+  output: false
 */
 
 /*
   Add written explanation of your solution here
+  the string will be splited using split, reverse and join methods to evaluate the condition given
 */
 
 // You can run `node index.js` to view these console logs
